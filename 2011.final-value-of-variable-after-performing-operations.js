eeeab00f592425395622067409216654a0cad67e
“Hidemichi <hidemichi.shimura@gmail.com>
2022-09-20 19:12:50 -0700

    solved 2011.Final Value of Variable After Performing Operations

---
+++ 2011.final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,27 @@
+/*
+ * @param {string[]} operations
+ * @return {number}
+ */
+
+/* URL of this problem
+    https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+*/
+
+var finalValueAfterOperations = function(operations) {
+    let X = 0;
+    
+    for (let i = 0; i < operations.length; i++) {
+        switch (operations[i]) {
+            case "--X":
+            case "X--":
+                X -= 1
+                break;
+            case "++X":
+            case "X++":
+                X += 1;
+                break;
+        }
+    }
+    
+    return X;
+};
