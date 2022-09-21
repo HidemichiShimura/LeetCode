c92d3c636174ac66d0d657dc7025e3153d618689
“Hidemichi <hidemichi.shimura@gmail.com>
2022-09-20 19:05:36 -0700

    solved 1920.Build Array from Permutation

---
+++ 1920.build-array-from-permutation.js
@@ -0,0 +1,18 @@
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+*/
+
+/* URL of this problem
+    https://leetcode.com/problems/build-array-from-permutation/
+*/
+
+ var buildArray = function(nums) {
+    let ans = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(nums[nums[i]]);
+    }
+    
+    return ans
+};
