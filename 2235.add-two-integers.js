bf061455380ebd23d12089da1df2ea74189ec0e7
“Hidemichi <hidemichi.shimura@gmail.com>
2022-09-22 16:26:02 -0700

    solved 2235. Add Two Integers

---
+++ 2235.add-two-integers.js
@@ -0,0 +1,13 @@
+/*
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+
+/* URL of this problem
+    https://leetcode.com/problems/add-two-integers/
+*/
+
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
