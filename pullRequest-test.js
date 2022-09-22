bfed27538ed3b871d3e7e66ec812667f0cb6ea94
“Hidemichi <hidemichi.shimura@gmail.com>
2022-09-21 21:13:57 -0700

    deleted pullRequest-test.js

--- pullRequest-test.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("btn");
-btn.addEventListener("click", changeColor);
-
-function changeColor() {
-  const test = document.querySelector("h1");
-  test.style.color = "white";
-}
