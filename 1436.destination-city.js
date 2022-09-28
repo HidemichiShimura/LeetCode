eda184e1c85b3f5ed5b7637e26bc346589bfd32e
“Hidemichi <hidemichi.shimura@gmail.com>
2022-09-28 10:58:11 -0700

    Change the initialization of the const variable pathTable

--- 1436.destination-city.js
+++ 1436.destination-city.js
@@ -8,7 +8,7 @@
 */
 
 var destCity = function(paths) {
-    const pathTable = {};
+    const pathTable = new Map();
     for (let i = 0; i < paths.length; i++) {
         pathTable[paths[i][0]] = paths[i][1];
     }
