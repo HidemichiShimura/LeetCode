61952949de219c772e90f1b8b9c5a1f1f00d189c
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-01 10:03:20 -0700

    solve 1304. Find N Unique Integers Sum up to Zero

---
+++ 1304.find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,22 @@
+/* URL of this problem
+    https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+*/
+
+/*
+ * @param {number} n
+ * @return {number[]}
+ */
+
+var sumZero = function(n) {
+    const uniqueIntArr = [];
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        const randomNum = i + 5;
+        uniqueIntArr.push(randomNum, randomNum * -1);
+    }
+    
+    if (n % 2 !== 0) {
+        uniqueIntArr.push(0);
+    }
+    
+    return uniqueIntArr
+};
