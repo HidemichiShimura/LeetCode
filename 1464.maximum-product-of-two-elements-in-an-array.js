1f6ab199e34e62bdeea467422a6d4db9f46db018
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-02 13:54:47 -0700

    modify reassignment of the varialbe maxValue

--- 1464.maximum-product-of-two-elements-in-an-array.js
+++ 1464.maximum-product-of-two-elements-in-an-array.js
@@ -13,9 +13,7 @@ var maxProduct = function(nums) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             const currValue = (nums[i]-1) * (nums[j]-1);
-            if (maxValue < currValue) {
-                maxValue = currValue;
-            }
+            maxValue = Math.max(maxValue, currValue);
         }
     }
     
