f0bd73283dfa2c076564825b5a91cb5e4df7ae3d
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-11 19:11:41 -0700

    modify 1304.find-n-unique-integers-sum-up-to-zero.js

--- 1304.find-n-unique-integers-sum-up-to-zero.js
+++ 1304.find-n-unique-integers-sum-up-to-zero.js
@@ -10,8 +10,8 @@
 var sumZero = function(n) {
     const uniqueIntArr = [];
     for (let i = 0; i < Math.floor(n / 2); i++) {
-        const randomNum = i + 5;
-        uniqueIntArr.push(randomNum, randomNum * -1);
+        const uniqueNum = i + 5;
+        uniqueIntArr.push(uniqueNum, uniqueNum * -1);
     }
     
     if (n % 2 !== 0) {
