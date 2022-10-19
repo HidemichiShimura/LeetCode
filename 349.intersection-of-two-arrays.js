00309af2ee085a83d472961756ace2de9d14c952
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-19 16:48:45 -0700

    modify 349. Intersection of Two Arrays

--- 349.intersection-of-two-arrays.js
+++ 349.intersection-of-two-arrays.js
@@ -7,15 +7,15 @@
  */
  
 var intersection = function(nums1, nums2) {
-    const distincetiveNums1 = [...new Set(nums1)];
-    const distincetiveNums2 = [...new Set(nums2)];
+    const uniqueNums1 = [...new Set(nums1)];
+    const uniqueNums2 = [...new Set(nums2)];
     const intersections = [];
-    
-    for (let i = 0; i < distincetiveNums1.length; i++) {
-        if (distincetiveNums2.includes(distincetiveNums1[i])) {
-            intersections.push(distincetiveNums1[i]);
+
+    for (let i = 0; i < uniqueNums1.length; i++) {
+        if (uniqueNums2.includes(uniqueNums1[i])) {
+            intersections.push(uniqueNums1[i]);
         }
     }
-    
+
     return intersections;
 };
