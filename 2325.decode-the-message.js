aa133e1d3df00bf46b1ba168051cbff5303ef8db
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-18 19:21:48 -0700

    avoid the side effect, add a function and optimize the if-else statement with the ternary operator

--- 2325.decode-the-message.js
+++ 2325.decode-the-message.js
@@ -9,26 +9,34 @@
  */
 
 var decodeMessage = function(key, message) {
-    key = key.replaceAll(" ", "");
-    const keyAlphabetMap = new Map();
-    let utfSixteenDecimal = 0;
-    for (let i = 0; i < key.length; i++) {
-        const currKey = key.charAt(i);
-        if (!keyAlphabetMap.has(currKey)) {
-            keyAlphabetMap.set(currKey, String.fromCharCode(97 + utfSixteenDecimal));
-            utfSixteenDecimal++;
-        }
-    }
-
+    const cipherKeys = key.replaceAll(" ", "");    
+    const cipherKeyAlphabetMap = makeCipherKeyMap(cipherKeys);    
     let decodedMsg = "";
+    
     for (let i = 0; i < message.length; i++) {
         const msgChar = message.charAt(i);
-        if (keyAlphabetMap.has(msgChar)) {
-            decodedMsg += keyAlphabetMap.get(msgChar);
-        } else {
-            decodedMsg += msgChar;
-        }
+        decodedMsg += cipherKeyAlphabetMap.has(msgChar) ? cipherKeyAlphabetMap.get(msgChar) : msgChar;
     }
     
     return decodedMsg;
+};
+
+function makeCipherKeyMap(cipherKeys) {
+    const map = new Map();
+    let utfSixteenDecimal = 0;
+    
+    for (let i = 0; i < cipherKeys.length; i++) {
+        const currKey = cipherKeys.charAt(i);
+        
+        // Skip the current iteration if the cipher key already exists on the map
+        if (map.has(currKey)) {
+            continue;
+        }
+        
+        // Get a lowercase English alphabet in the UTF-16 table using decimal and set the alphabet for value
+        map.set(currKey, String.fromCharCode(97 + utfSixteenDecimal));
+        utfSixteenDecimal++;
+    }
+
+    return map;   
 };
