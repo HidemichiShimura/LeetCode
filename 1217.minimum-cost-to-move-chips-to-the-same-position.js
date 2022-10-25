38c78282c303dc8d342dc0c61c9a7bc9e475ff6d
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-25 13:20:46 -0700

    solve 1217. Minimum Cost to Move Chips to The Same Position

---
+++ 1217.minimum-cost-to-move-chips-to-the-same-position.js
@@ -0,0 +1,13 @@
+/* URL of this problem
+ * https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+ *
+ * @param {number[]} position
+ * @return {number}
+ */
+ 
+var minCostToMoveChips = function(position) {
+    const chipsAtOddIndices = position.filter(num => num % 2 === 1);
+    const chipsAtEvenIndices = position.filter(num => num % 2 === 0);
+    
+    return Math.min(chipsAtOddIndices.length, chipsAtEvenIndices.length);
+};
