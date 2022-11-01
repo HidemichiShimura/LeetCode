7e8f5d16acc314a809b09b8ccaa3c4ab99929611
“Hidemichi <hidemichi.shimura@gmail.com>
2022-10-31 23:38:07 -0700

    modify 485. Max Consecutive Ones

--- 485.max-consecutive-ones.js
+++ 485.max-consecutive-ones.js
@@ -6,18 +6,8 @@
  */
  
 var findMaxConsecutiveOnes = function(nums) {
-    let maxConsecutiveOneNum = 0;
-    // Count consecutive ones temporarily until 0 is reached
-    let tempNum = 0;
+    const consecutiveOnes = nums.join("").split(0);
+    const consecutiveOneLenghts = consecutiveOnes.map(element => element.length);
     
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 1) {
-            tempNum++;
-            maxConsecutiveOneNum = Math.max(tempNum ,maxConsecutiveOneNum);
-        } else {
-            tempNum = 0;
-        }
-    }
-    
-    return maxConsecutiveOneNum;
+    return Math.max(...consecutiveOneLenghts);
 };
