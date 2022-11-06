8b6ef0b042f8f2398c1e549e855603e121c6542e
“Hidemichi <hidemichi.shimura@gmail.com>
2022-11-06 10:09:43 -0800

    solve 2169. Count Operations to Obtain Zero

---
+++ 2169.count-operations-to-obtain-zero.js
@@ -0,0 +1,25 @@
+/* URL of this problem
+ * https://leetcode.com/problems/count-operations-to-obtain-zero/
+ *
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+ 
+var countOperations = function(num1, num2) {
+    let operationCount = 0;
+    
+    while (num1 !== 0 && num2 !== 0) {
+        if (num1 >= num2) {
+            num1 -= num2;
+        } else {
+            num2 -= num1;
+        }
+        
+        operationCount++;
+    }
+    
+    return operationCount;
+};
+
+module.exports = countOperations;
