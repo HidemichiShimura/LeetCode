5313286b23afc8a921f4b59da856d424ade70298
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-02-05 10:29:32 -0800

    solve 504. Base 7

---
+++ 504.base-7.js
@@ -0,0 +1,12 @@
+/* URL of this problem
+ * https://leetcode.com/problems/base-7/description/
+ *
+ * @param {number} num
+ * @return {string}
+ */
+
+var convertToBase7 = function (num) {
+  return num.toString(7);
+};
+
+module.exports = convertToBase7;
