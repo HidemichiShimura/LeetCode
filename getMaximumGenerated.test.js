5300a2307e16087368c07318a94e0bc4283d6b89
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-02-13 15:24:32 -0800

    test function getMaximumGenerated

---
+++ getMaximumGenerated.test.js
@@ -0,0 +1,17 @@
+const getMaximumGenerated = require("./1646.get-maximum-in-generated-array");
+
+test("Return the maximum integer in the array nums", () => {
+  expect(getMaximumGenerated(7)).toBe(3);
+});
+
+test("Return 0 if n is 0", () => {
+  expect(getMaximumGenerated(0)).toBe(0);
+});
+
+test("Return 1 if n is 1", () => {
+  expect(getMaximumGenerated(1)).toBe(1);
+});
+
+test("Return 1 if n is 1", () => {
+  expect(getMaximumGenerated(-1)).toBe(1);
+});
