29a98a19ee35f813e5b93787b57e0e0f113fdeca
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-03-10 10:19:47 -0800

    solve 28. Find the Index of the First Occurrence in a String

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,12 @@
+/* URL of this problem
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  return haystack.indexOf(needle);
+};
+
+module.exports = strStr;
