2f555eefbc186a0dc08482653cae979467a6d069
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-10-09 11:49:18 -0700

    solve 2733. Neither Minimum nor Maximum

---
+++ 2733.neither-minimum-nor-maximum.js
@@ -0,0 +1,11 @@
+/* URL of this problem
+ * https://leetcode.com/problems/neither-minimum-nor-maximum/description/
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function (nums) {
+  return nums.length > 2 ? [...nums].sort((a, b) => a - b)[1] : -1;
+};
+
+module.exports = findNonMinOrMax;
