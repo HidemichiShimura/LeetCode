311ebdc32dd8bd31227ddab0ecc63e9c82d11bf2
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-11-02 07:22:20 -0700

    solve: 2769. Find the Maximum Achievable Number

---
+++ 2769/theMaximumAchievableX.js
@@ -0,0 +1,12 @@
+/* URL of this problem
+ * https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+ *
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function (num, t) {
+  return num + t * 2;
+};
+
+module.exports = theMaximumAchievableX;
